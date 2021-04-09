c1238f619998298c909eaddd950c7e27177e1aa4
yusewaki <sewayu.pc11@gmail.com>
2021-04-09 06:22:17 +0000

    [add]4-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected # 呼び出された他のコントローラからも参照可能
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  # ユーザ登録(sign_up)の際に、ユーザ名(name)のデータ操作が許可される
 end
