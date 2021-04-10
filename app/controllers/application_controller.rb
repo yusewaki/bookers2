44aa8fd2e56dc0695eda10690acefc2defe6a80d
yusewaki <sewayu.pc11@gmail.com>
2021-04-10 03:20:41 +0000

    [add]4-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resources)
+    user_path(@user.id)
+  end
+
   protected # 呼び出された他のコントローラからも参照可能
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'homes#top'
+  get '/home/about' => 'homes#about'
+
+  resources :users
+  resources :books
 end
