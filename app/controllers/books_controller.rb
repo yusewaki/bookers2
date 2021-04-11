c6eeb976d831a5f9457a92746766a9e684a83834
yusewaki <sewayu.pc11@gmail.com>
2021-04-11 19:16:14 +0000

    [add]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render :index
@@ -44,7 +44,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
-      redirect_to book_path(book.id)
+      redirect_to book_path(book.id), notice: "You have updated book successfully."
     else
       @book = book
       render :edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   private
