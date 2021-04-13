43013cd4c062934093650d1954e5e2b186036804
yusewaki <sewayu.pc11@gmail.com>
2021-04-13 05:53:07 +0000

    [add]4-11

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/home/about' => 'homes#about'
 
   resources :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 
   post '/users/:id' => 'books#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_061720) do
+ActiveRecord::Schema.define(version: 2021_04_13_051113) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
